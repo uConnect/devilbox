38c5de93cedeb5c39c9b5267def1a819e7a4f88f
Parham Ghaffarian <1007718+ParhamG@users.noreply.github.com>
2023-06-29 13:36:15 -0400

    Added default auth key and salts

--- data/www/uconnectlabs/wp-config-env-dev-sample.php
+++ data/www/uconnectlabs/wp-config-env-dev-sample.php
@@ -57,3 +57,23 @@ define( 'DEV_LOCAL_SERVER_DOMAIN', DEV_LOCAL_SERVER_USER . '.dev.loc.uconnectlab
 define( 'DEV_BRIDGE_ONLY_RUN_MANUAL_IMPORTS', false );
 
 define( 'QM_DISABLE_ERROR_HANDLER', true );
+
+/**#@+
+ * Authentication unique keys and salts.
+ *
+ * Change these to different unique phrases! You can generate these using
+ * the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}.
+ *
+ * You can change these at any point in time to invalidate all existing cookies.
+ * This will force all users to have to log in again.
+ *
+ * @since 2.6.0
+ */
+define('AUTH_KEY',         'Div_i|wN:wpqJ)&GF=#@w^!0GP>}bcl>U5WQ-H/|mat^_-Jm8cUp8~t[@QfeU6$4');
+define('SECURE_AUTH_KEY',  'H,$>{9A%-RWF%_o+HojZWcq%[}w!P,M{81:.sM35SWKUGu[BNhkcS 0BUU+Ww/iP');
+define('LOGGED_IN_KEY',    '/rQIqP{nVD+{wX5>.O*c?d_0KD964uPkVSk3]uuS054.;#reV7d_k:s5|RKawJoX');
+define('NONCE_KEY',        'fuK@:l^4fhwcH= ,*FUln{(<sXZ9v$POQ;nBX0Txn&~<:gEJzq/b:*.?nsX[jv1m');
+define('AUTH_SALT',        '@IzK_Gv2Ix`wN40V%hLp3GRM`<8iR/*<)(Hgqzh|@bx^<3y|R~;unLuW5 #aaALb');
+define('SECURE_AUTH_SALT', 'e->ecBIslm<0Vhzj#lbeXxW%@MGXy)Je24+J]QGTy N&D/YPL~/<}z]R+3A+O+iy');
+define('LOGGED_IN_SALT',   '3QIga!O?`Exo1,csG9h2_Oi+}(+IV2xsH .KjhOX)+@|@tn-lV NyGoVGS4M)9!;');
+define('NONCE_SALT',       'dvPJP|-,IBdcgfTw6 qFG?af>rvBwoPJ;<h:yq7xV$B1CW+V&ws-$|`k6b*EjZNN');
